08db85b7bdf0960555d6cd038395912918e12353
Sam White <samuelwhite98@outlook.com>
2022-08-21 16:11:09 +0100

    Add button and function to change grid size

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function createGrid(){
-
-    for (y = 0; y < 16; y++){
+function createGrid(inputSize = 32){
+    
+    for (y = 0; y < inputSize; y++){
 
         const container = document.querySelector(".grid-container")
         let row = document.createElement("div")
         row.classList.add(`grid-row-${y}`)
         container.appendChild(row)
 
-        for (x = 0; x < 16; x++){
+        for (x = 0; x < inputSize; x++){
 
             const container = document.querySelector(`.grid-row-${y}`);
             let div = document.createElement("div")
@@ -15,14 +15,30 @@ function createGrid(){
             container.appendChild(div)
         }
     }
-    
+
+    divs = document.querySelectorAll(".grid-div")
+    divs.forEach(div => div.addEventListener('mouseover', function(e){
+    e.target.style.background = "black"
+    }))   
 }
 
 
+buttonSize = document.querySelector(".button-size")
+buttonSize.addEventListener('click', function(){
+    inputSize = prompt("Please enter a grid size - e.g. 48 will create a 48x48 grid. Maximum number is 100")
+    if (inputSize > 100){
+        alert("Maximum value is 100")
+        return;
+    }
+    const container = document.querySelector('.grid-container');
+    removeAllChildNodes(container);
+    createGrid(inputSize)
+})
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 createGrid()
-
-divs = document.querySelectorAll(".grid-div")
-divs.forEach(div => div.addEventListener('mouseover', function(e){
-    e.target.style.background = "black"
-}))
